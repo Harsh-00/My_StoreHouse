ccd8c3c90040d6b39ed86d2091cbf49449fec52e
Harsh Agarwal <harshulkh@gmail.com>
2024-12-26 02:38:25 +0530

    crud

--- main.py
+++ main.py
@@ -20,12 +20,27 @@ app.add_middleware(
 def read_root():
     return {"My":"Store"}
 
-@app.post("/products")
+
+@app.post("/product")
 def create_product(product:Product):
     return product.save()
 
 
+@app.get("/product/all")
+def get_all_products():
+    return [detailed_product(pk) for pk in Product.all_pks()]
+def detailed_product(pk:str):
+    product= Product.get(pk)
+    return product.dict()
 
 
+@app.get("/product/{pk}")
+def get_product(pk:str):
+    return Product.get(pk)
+
+
+@app.delete("/product/{pk}")
+def delete_product(pk:str):
+    return Product.delete(pk)
 
 
